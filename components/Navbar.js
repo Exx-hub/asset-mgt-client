a87c40935a7e4e27ac8fb2acb64f75876de534a3
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-26 17:56:22 +0800

    Install next themes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,4 @@
-const Navbar = () => {
+const NavBar = () => {
 	return (
 		<div>
 			<h1>NAVBAR</h1>
@@ -6,4 +6,4 @@ const Navbar = () => {
 	);
 };
 
-export default Navbar;
+export default NavBar;
